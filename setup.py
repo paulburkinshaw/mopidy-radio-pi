9a93dad15a2e52d41b9475dd83f66a68d160a4c2
paulburkinshaw <paul.burkinshaw@gmail.com>
2015-03-01 07:33:22 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,49 @@
+from __future__ import unicode_literals
+
+import re
+
+from setuptools import find_packages, setup
+
+
+def get_version(filename):
+    content = open(filename).read()
+    metadata = dict(re.findall("__([a-z]+)__ = '([^']+)'", content))
+    return metadata['version']
+
+
+setup(
+    name='Mopidy-Radio-Pi',
+    version=get_version('mopidy_radio-pi/__init__.py'),
+    url='https://github.com/paulburkinshaw/mopidy-radio-pi',
+    license='Apache License, Version 2.0',
+    author='Paul Burkinshaw',
+    author_email='paul.burkinshaw@gmail.com',
+    description='online radio http interface for the Mopidy music server',
+    long_description=open('README.rst').read(),
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=[
+        'setuptools',
+        'Mopidy >= 0.19',
+        'Pykka >= 1.1',
+    ],
+    test_suite='nose.collector',
+    tests_require=[
+        'nose',
+        'mock >= 1.0',
+    ],
+    entry_points={
+        'mopidy.ext': [
+            'radio-pi = mopidy_radio-pi:Extension',
+        ],
+    },
+    classifiers=[
+        'Environment :: No Input/Output (Daemon)',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Topic :: Multimedia :: Sound/Audio :: Players',
+    ],
+)
