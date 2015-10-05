3da2fee50d77c03871fbfa21c4c058bdec1aa003
paulburkinshaw <paul.burkinshaw@gmail.com>
2015-10-05 20:22:23 +0100

    Backend stuff

--- mopidy_radio_pi/radiopidb.py
+++ mopidy_radio_pi/radiopidb.py
@@ -12,10 +12,10 @@ with con:
     
 		cur = con.cursor()   
         cur.execute("DROP TABLE IF EXISTS Tracklist")
-		cur.execute("CREATE TABLE Tracklist(Id INTEGER PRIMARY KEY, TrackListTitle TEXT, TrackListDescription TEXT, TrackListThemeId INT, TrackListDate DATE)")
+		cur.execute("CREATE TABLE Tracklist(Id INTEGER PRIMARY KEY, TrackListThemeId INT, PlaylistUri TEXT, TrackListTitle TEXT, TrackListDescription TEXT, TrackListDate DATE, IsCurrent INT)")
 		
 		cur.execute("DROP TABLE IF EXISTS TracklistTracks")
-		cur.execute("CREATE TABLE Tracklist(Id INTEGER PRIMARY KEY, TracklistId INT, TrackTitle TEXT, TrackArtist TEXT, TrackUri TEXT, ChosenBy TEXT, DedicatedTo TEXT, Comments TEXT, DateAdded DATE, Username TEXT)")
+		cur.execute("CREATE TABLE Tracklist(Id INTEGER PRIMARY KEY, TracklistId INT, PlaylistUri TEXT, TrackTitle TEXT, TrackArtist TEXT, TrackUri TEXT, ChosenBy TEXT, DedicatedTo TEXT, Comments TEXT, DateAdded DATE, Username TEXT)")
 		
         cur.execute("DROP TABLE IF EXISTS CurrentTrack")
 		cur.execute("CREATE TABLE Tracklist(Id INTEGER PRIMARY KEY, TracklistId INT, TrackUri TEXT)")
@@ -32,8 +32,15 @@ with con:
 		cur.execute("DROP TABLE IF EXISTS VoteToSkip")
 		cur.execute("CREATE TABLE VoteToSkip(Id INTEGER PRIMARY KEY, TrackUri TEXT, TracklistId INT, Comments TEXT, DateVoted DATE, HostAddress TEXT, Username TEXT)")
 		
-		cur.execute("DROP TABLE IF EXISTS User")
-		cur.execute("CREATE TABLE User(Id INTEGER PRIMARY KEY, UserName TEXT, PiPoints INT, TracksRequested INT, TracksSkipVoted INT, TracksSucessfullySkipped INT, TracksLiked INT)")
+		cur.execute("DROP TABLE IF EXISTS UserProfile")
+		cur.execute("CREATE TABLE User(Id INTEGER PRIMARY KEY, UserName TEXT, FirstName TEXT, Surname TEXT, NickName TEXT, Bio TEXT, ProfilePicture TEXT, Vibes INT, LevelId INT, TracksRequested INT, TracksSkipVoted INT, TracksSucessfullySkipped INT, TracksLiked INT)")
 		
+        cur.execute("DROP TABLE IF EXISTS FavouriteTracks")
+		cur.execute("CREATE TABLE Tracklist(Id INTEGER PRIMARY KEY, TrackUri TEXT, TrackTitle TEXT, TrackArtist TEXT, DateAdded DATE, Username TEXT)")
+		
+        cur.execute("DROP TABLE IF EXISTS Level")
+		cur.execute("CREATE TABLE User(Id INTEGER PRIMARY KEY, LevelName TEXT, LevelDescription TEXT, LevelIcon TEXT, VibesRequired INT, TracksAllowed INT, VotePower INT, SkipPower INT)")
+		
+
 		#cur.execute("INSERT INTO Tracklist VALUES(1,'spotify:track:4ollMTGkoG7ytZ7oB9SbyC','pburkinshaw',date('now'))")
 		#cur.execute("INSERT INTO Tracklist VALUES(2,'spotify:track:0bVyjkbYDx6QeHrCoHuoxY','anonymous',date('now'))")
