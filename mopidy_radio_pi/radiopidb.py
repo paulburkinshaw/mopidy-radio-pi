18ed5d56d4f6c0f6d88de64a0fe73b118c0e929c
paulburkinshaw <paul.burkinshaw@gmail.com>
2015-10-06 22:03:20 +0100

    Theme table

--- mopidy_radio_pi/radiopidb.py
+++ mopidy_radio_pi/radiopidb.py
@@ -12,10 +12,7 @@ with con:
     
 		cur = con.cursor()   
         cur.execute("DROP TABLE IF EXISTS Tracklist")
-		cur.execute("CREATE TABLE Tracklist(Id INTEGER PRIMARY KEY, PlaylistUri TEXT, TrackListTitle TEXT, TrackListDescription TEXT, TrackListImage TEXT, TracklistUser TEXT, TrackListDate DATE, IsCurrent INT)")
-		
-        cur.execute("DROP TABLE IF EXISTS TrackListBanners")
-		cur.execute("CREATE TABLE Tracklist(Id INTEGER PRIMARY KEY, TracklistId INT, BannerImageFileName TEXT, BannerImagePath TEXT, DateAdded DATE)")
+		cur.execute("CREATE TABLE Tracklist(Id INTEGER PRIMARY KEY, ThemeId INT, PlaylistUri TEXT, TrackListTitle TEXT, TrackListDescription TEXT, TrackListImage TEXT, TracklistUser TEXT, TrackListDate DATE, IsCurrent INT)")
 		
 		cur.execute("DROP TABLE IF EXISTS TracklistTracks")
 		cur.execute("CREATE TABLE Tracklist(Id INTEGER PRIMARY KEY, TracklistId INT, PlaylistUri TEXT, TrackTitle TEXT, TrackArtist TEXT, TrackUri TEXT, ChosenBy TEXT, DedicatedTo TEXT, Comments TEXT, DateAdded DATE, Username TEXT, BeenPlayed INT)")
@@ -26,7 +23,7 @@ with con:
         cur.execute("DROP TABLE IF EXISTS TrackComments")
 		cur.execute("CREATE TABLE Tracklist(Id INTEGER PRIMARY KEY, TrackId INT, TrackUri TEXT, Comments TEXT, Username TEXT, DateAdded DATE,)")
 		
-       cur.execute("DROP TABLE IF EXISTS TrackLikes")
+        cur.execute("DROP TABLE IF EXISTS TrackLikes")
 		cur.execute("CREATE TABLE UpVote(Id INTEGER PRIMARY KEY, TrackId INT, TrackUri TEXT, TracklistId INT, Comments TEXT, DateLiked DATE, HostAddress TEXT, Username TEXT)")
 					
 		cur.execute("DROP TABLE IF EXISTS TrackSkips")
@@ -43,6 +40,13 @@ with con:
 		
         cur.execute("DROP TABLE IF EXISTS Level")
 		cur.execute("CREATE TABLE User(Id INTEGER PRIMARY KEY, LevelName TEXT, LevelDescription TEXT, LevelIcon TEXT, VibesRequired INT, TracksAllowed INT, VotePower INT, SkipPower INT)")
+		
+        cur.execute("DROP TABLE IF EXISTS Theme")
+		cur.execute("CREATE TABLE User(Id INTEGER PRIMARY KEY, ThemeTitle TEXT, ThemeDescription TEXT, ThemeImage TEXT)")
+
+        cur.execute("DROP TABLE IF EXISTS ThemeBanners")
+		cur.execute("CREATE TABLE Tracklist(Id INTEGER PRIMARY KEY, ThemeId INT, BannerImageFileName TEXT, BannerImagePath TEXT, DateAdded DATE)")
+		
 		
 
 		#cur.execute("INSERT INTO Tracklist VALUES(1,'spotify:track:4ollMTGkoG7ytZ7oB9SbyC','pburkinshaw',date('now'))")
